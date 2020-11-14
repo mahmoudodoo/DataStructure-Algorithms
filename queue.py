df5ecbf43af1798b8fa8841b6ff9dcf69f86d6ec
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-11-14 13:34:13 +0200

    Create new python file and queue class and apply the queue on List

---
+++ queue.py
@@ -0,0 +1,45 @@
+"""Make a Queue class using a list!
+Hint: You can use any Python list method
+you'd like! Try to write each one in as 
+few lines as possible.
+"""
+
+# Create Queeu Class and apply it on List 
+class Queue(object):
+    def __init__(self, head=None):
+        self.storage = [head]
+    # add new element to the queue
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+    # Return the first element
+    def peek(self):
+        return self.storage[0]
+    # Delete the first element from the list (dequeue)
+    def dequeue(self):
+        return self.storage.pop(0)
+
+
+# Setup
+q = Queue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test peek
+# Should be 1
+print q.peek()
+
+# Test dequeue
+# Should be 1
+print q.dequeue()
+
+# Test enqueue
+q.enqueue(4)
+# Should be 2
+print q.dequeue()
+# Should be 3
+print q.dequeue()
+# Should be 4
+print q.dequeue()
+q.enqueue(5)
+# Should be 5
+print q.peek()
